9477094b84489c941976b197ae8f91a6e350ab0a
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-04-03 21:13:20 -0300

    Nested if
    Resized the images and added a nested ifs to improve the code.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function verify() {
         //Verify if is a valid year
        if (yearProvided < year) 
        {
-        divTxt.textContent = `Your ${brand} pickup is of ${yearsOld}`
+        divTxt.textContent = `Your pickup is a ${brand} of ${yearsOld}`
         }     
             else 
         {
@@ -27,69 +27,64 @@ function verify() {
         }  
 
         //Chevrolet cars
-    if (brand = 'CHEVROLET')
-    {
-        if (yearsOld < 1960)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1950_f.jpg)"
+        if (brand == 'CHEVROLET') {
+            if (yearsOld < 1960) {
+                imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1950_f.jpg)"
+            } else {
+                if (yearsOld < 1970) {
+                    imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1960_f.jpg)"
+                } else {
+                    if (yearsOld < 1980) {
+                        imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1970_f.jpg)"
+                    } else {
+                        if (yearsOld < 1990) {
+                            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1980_f.jpg)"
+                        } else {
+                            if (yearsOld < 2000) {
+                                imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1990_f.jpg)"
+                            } else {
+                                if (yearsOld < 2010) {
+                                    imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-2000_f.jpg)"
+                                } else {
+                                    imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-2010_f.jpg)"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+        //FORD
+        else {
+            if (yearsOld < 1960) {
+                imageDiv = imageDiv.style.backgroundImage = "url(image/ford-1950_f.jpg)"
+            } else {
+                if (yearsOld < 1970) {
+                    imageDiv = imageDiv.style.backgroundImage = "url(image/ford-1960_f.jpg)"
+                } else {
+                    if (yearsOld < 1980) {
+                        imageDiv = imageDiv.style.backgroundImage = "url(image/ford-1970_f.jpg)"
+                    } else {
+                        if (yearsOld < 1990) {
+                            imageDiv.style.backgroundImage = "url(image/ford-1980_f.jpg)"
+                        } else {
+                            if (yearsOld < 2000) {
+                                imageDiv.style.backgroundImage = "url(image/ford-1990_f.jpg)"
+                            } else {
+                                if (yearsOld < 2010) {
+                                    imageDiv.style.backgroundImage = "url(image/ford-2000_f.jpg)"
+                                } else {
+                                    if (yearsOld < 2020) {
+                                        imageDiv.style.backgroundImage = "url(image/ford-2010_f.jpg)"
+                                    } else {
+                                        imageDiv.style.backgroundImage = "url(image/ford-2020_f.jpg)"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        else if (yearsOld >= 1960 && yearsOld < 1970)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1960_f.jpg)"
-        }
-        else if (yearsOld >= 1970 && yearsOld < 1980)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1970_f.jpg)"
-        }
-        else if (yearsOld >= 1980 && yearsOld < 1990)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1980_f.jpg)"
-        }
-        else if (yearsOld >= 1990 && yearsOld < 2000)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-1990_f.jpg)"
-        }
-        else if (yearsOld >= 2000 && yearsOld < 2010)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-2000_f.jpg)"
-        }
-        else if (yearsOld >= 2010)
-        {
-            imageDiv = imageDiv.style.backgroundImage = "url(image/chevrolet-2010_f.jpg)"
-        }
+        
     }
-        //Ford cars
-        else
-            if (yearsOld < 1960)
-            {
-                imageDiv = imageDiv.style.backgroundImage = "url(image/ford-1950.jpg)"
-            }
-            else if (yearsOld >= 1960 && yearsOld < 1970)
-            {
-                imageDiv = imageDiv.style.backgroundImage = "url(image/ford-1960.jpg)"
-            }
-            else if (yearsOld >= 1970 && yearsOld < 1980)
-            {
-                imageDiv = imageDiv.style.backgroundImage = "url(image/ford-1970.jpg)"
-            }
-            else if (yearsOld >= 1980 && yearsOld < 1990)
-            {
-                imageDiv.style.backgroundImage = "url(image/ford-1980.jpg)"
-            }
-            else if (yearsOld >= 1990 && yearsOld < 2000)
-            {
-                imageDiv.style.backgroundImage = "url(image/ford-1990.jpg)"
-            }
-            else if (yearsOld >= 2000 && yearsOld < 2010)
-            {
-                imageDiv.style.backgroundImage = "url(image/ford-2000.jpg)"
-            }
-            else if (yearsOld >= 2010 && yearsOld < 2020)
-            {
-                imageDiv.style.backgroundImage = "url(image/ford-2010.jpg)"
-            }
-            else 
-            {
-                imageDiv.style.backgroundImage = "url(image/ford-2020.jpg)"
-            }
-}
